1d2c5e703deea86c04d342495acc63ae0f48f440
yuki <10756044@ntub.edu.tw>
2022-01-20 15:24:08 +0800

    feat(coffees):add error handlers

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wz+_1givy=hgvq4+-ino)c8x_!7pwxzgj0+@rd+eqoro3^0($t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
