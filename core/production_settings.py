0ff0a9572541a73e1d1ee49e4044c1e7855871ba
yuki <10756044@ntub.edu.tw>
2022-01-24 10:05:40 +0800

    chore(heroku):set up new

--- core/production_settings.py
+++ core/production_settings.py
@@ -1,3 +1,5 @@
+import os
+
 import dj_database_url
 
 
@@ -10,3 +12,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True),
 }
+
+EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER',None)
+
+EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD',None)
